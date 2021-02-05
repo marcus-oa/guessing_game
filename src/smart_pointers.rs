3d68ecc70e704251b07f143504a948162df2cc5b
marcus_oa <marcus.official1@gmail.com>
2021-02-05 20:54:38 +0000

    https://doc.rust-lang.org/book/ch15-05-interior-mutability.html - Started

--- src/smart_pointers.rs
+++ src/smart_pointers.rs
@@ -138,10 +138,16 @@ pub fn drop_example() {
 //               /
 // c ------> |4| |
 
-fn rc_example() {
+pub fn rc_example() {
     // required to be created as a Rc::new instance instead of Cons(x,..) to be referenced
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
     // Note: Rc::clone only increases the reference count
     let b = Cons(3, Rc::clone(&a));
-    let c = Cons(4, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
